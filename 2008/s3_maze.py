09fe32825ddeb7a71a55bd5d6876328c3fac264a
henhenmao <henrymao54321@gmail.com>
2024-11-27 12:26:51 -0800

    finihsed 2009 s3 maze

--- 2008/s3_maze.py
+++ 2008/s3_maze.py
@@ -1,57 +1,57 @@
 
-# NOT SOLVED STILL DOES NOT GIVE FULL MARKS DUE TO TLE
-# DID NOT OPTIMIZE CASE OF 20x20 CROSS INTERSECTIONS
 
-def bfs(grid, rows, cols): # outputs min path
+cross_dir = [(-1,0),(1,0),(0,1),(0,-1)]
+ver_dir = [(-1,0), (1,0)]
+hor_dir = [(0,-1), (0,1)]
+
+def bfs(grid, rows, cols):
     
-    def valid_move(row, col):
-        if row < 0 or row >= r or col < 0 or col >= c: # out of bounds
+    visited = [[False for _ in range(cols)] for _ in range(rows)]
+    visited[0][0] = True
+    queue = [(0,0,1)] # (x, y, distance) for each node
+
+    def validMove(x, y):
+        if x < 0 or x >= r or y < 0 or y >= c:
             return False
-        if visited[row][col]: # already visited
+        if visited[x][y]:
+            return False
+        if grid[x][y] == "*":
             return False
         return True
     
-    visited = [[False for _ in range(cols)] for _ in range(rows)]
-    queue = [(0,0,1)] # queue stores (x, y, distance)
-
-    cross_dir = [(-1,0),(1,0),(0,1),(0,-1)]
-    ver_dir = [(-1,0), (1,0)]
-    hor_dir = [(0,-1), (0,1)]
-
     while queue:
         curr = queue.pop(0)
         curr_x = curr[0]
         curr_y = curr[1]
         curr_distance = curr[2]
-        current_road = grid[curr_x][curr_y]
+        curr_road = grid[curr_x][curr_y]
 
         if curr_x == rows-1 and curr_y == cols-1:
             return curr_distance
-        
 
-        if current_road == "*":
+        if curr_road == "*":
             continue
-        if current_road == "+":
+        elif curr_road == "+":
             curr_dir = cross_dir
-        elif current_road == "|":
+        elif curr_road == "|":
             curr_dir = ver_dir
-        elif current_road == "-":
+        elif curr_road == "-":
             curr_dir = hor_dir
         
-        for dir in curr_dir:
-            next_x = curr_x + dir[0]
-            next_y = curr_y + dir[1]
-            if valid_move(next_x, next_y) and not visited[next_x][next_y]:
+        for d in curr_dir:
+            next_x = curr_x+d[0]
+            next_y = curr_y+d[1]
+            if validMove(next_x, next_y):
                 visited[next_x][next_y] = True
                 queue.append((next_x, next_y, curr_distance+1))
     return -1
+                
 
 test_cases = int(input())
 for _ in range(test_cases):
     r, c = int(input()), int(input())
-
     grid = []
     for _ in range(r):
-        grid.append(input())
-
+        grid.append(list(input()))
+    
     print(bfs(grid, r, c))
